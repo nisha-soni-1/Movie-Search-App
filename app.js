eb42d2ec0005ac84a01fc28a77a90f8f73ca41a3
Nisha Soni <ns6433629@gmail.com>
2026-01-29 23:20:41 +0530

    Initial commit - Movie Search App

---
+++ app.js
@@ -0,0 +1,7 @@
+const API_KEY = '581144cc'; // <-- REPLACE with your key
+const BASE_URL = 'https://www.omdbapi.com/';
+
+const qInput = document.getElementById('query');
+const typeSelect = document.getElementById('type');
+const yearInput = document.getElementById('year');
+const searchBtn = document.getElementById('search');
